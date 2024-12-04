23f1c94a4045d2d4e2031488f11259d0d4633ec2
SaraK13 <sarahlanikai@gmail.com>
2024-12-04 21:21:13 +0100

    Home setup

--- towa/src/App.tsx
+++ towa/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Home from './pages/Home';
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <Home />
       <Footer />
     </>
   )
